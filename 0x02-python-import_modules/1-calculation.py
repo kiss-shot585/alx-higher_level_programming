85d4a1a30a65e622ec56237b06bc04163e1905b2
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-06-03 01:45:20 +0100

    python

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
+    """little calculator that can add, subtract, multiply, divide 10 and 5"""
     from calculator_1 import add, sub, mul, div
 
     a = 10
