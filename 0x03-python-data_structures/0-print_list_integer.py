0b2a6fddde15336ee717a0c8d5c135100c603ed5
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-06-06 23:43:39 +0100

    python

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     """To print a list of integers"""
-    print(f"list of integers is {my_list}\n".format(my_list=[])
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
