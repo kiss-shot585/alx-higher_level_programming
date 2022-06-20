6249fcea2222948988e45aa1f7b2fdfe44196969
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-06-20 18:31:12 +0100

    exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,10 @@
-python
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (ZeroDivisionError, ValueError, TypeError, IndexError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
