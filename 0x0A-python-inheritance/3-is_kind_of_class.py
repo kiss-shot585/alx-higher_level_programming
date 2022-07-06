1e7e9cd2a0439ff69be4e6b25fe85535076e7a79
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-07-06 07:41:19 +0100

    kind if class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,12 @@
-python
+#!/usr/bin/python3
+''' module: 2-is_same_class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''function: is_kind_of_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
+    '''
+    return isinstance(obj, a_class)
