0325519e4a3b5207be9195cb4d0ff24237407a76
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-09-05 22:02:28 +0100

    JS

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,8 @@
-empty
+#!/usr/bin/node
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
