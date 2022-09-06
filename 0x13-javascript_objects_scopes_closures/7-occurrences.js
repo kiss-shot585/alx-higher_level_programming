85631f972e942f8947ed0b46738e8319bcad267c
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-09-06 22:50:48 +0100

    JSC

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,6 @@
-empty
+#!/usr/bin/node
+// function that returns the number of occurrences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  return list.filter((value) => (value === searchElement)).length;
+};
