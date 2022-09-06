402f8f9e0e8fd4150f4cf5f05595a412660a26b7
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-09-06 22:51:21 +0100

    JSC

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,11 @@
-empty
+#!/usr/bin/node
+// function that returns the reversed version of a list
+
+exports.esrever = function (list) {
+
+  let rlist = [];
+  while (list.length > 0) {
+    rlist.push(list.pop());
+  }
+  return rlist;
+};
