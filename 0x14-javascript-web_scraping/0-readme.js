813dffa78f68444179b1e3ed7a5dac45d65705ff
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-10-18 15:35:41 +0100

    JS

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,13 @@
-JS
+#!/usr/bin/node
+// A script that reads and prints the content of a file
+
+const file = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
