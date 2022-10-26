3203d6bad98f308b39f152f0dc03eb150f14acd3
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-10-27 00:55:33 +0100

    JQ

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,9 @@
-JS
+// JavaScript script that displays the value of hello
+
+$(() => {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', (data, textStatus) => {
+    if (textStatus === 'success') {
+      $('div#hello').text(data.hello);
+    }
+  });
+});
